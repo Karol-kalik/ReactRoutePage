2143d5653f4fb14054e8e1d447591a179eccbee6
Karol Wąsik <karol.wasik00@gmail.com>
2020-08-13 10:22:47 +0200

    Updates

---
+++ precache-manifest.39163a2b9f4c304841651cdc56fa26f5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7df55a8131d51ab19377b557a5927c7a",
+    "url": "/ReactRoutePage/index.html"
+  },
+  {
+    "revision": "7c42979737c1e6583387",
+    "url": "/ReactRoutePage/static/css/main.7ce5b03f.chunk.css"
+  },
+  {
+    "revision": "ba166bebf624b92a71ed",
+    "url": "/ReactRoutePage/static/js/2.a22d10f3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ReactRoutePage/static/js/2.a22d10f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c42979737c1e6583387",
+    "url": "/ReactRoutePage/static/js/main.544ae665.chunk.js"
+  },
+  {
+    "revision": "468174354b67aa22cb56",
+    "url": "/ReactRoutePage/static/js/runtime-main.3da7157d.js"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/ReactRoutePage/static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/ReactRoutePage/static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/ReactRoutePage/static/media/header3.d9d36b65.jpg"
+  }
+]);
--- src/pages/ErrorPage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const ErrorPage = () => {
-  return (
-    <p> Błąd 404. Nie ma takiej strony </p>
-  )
-}
-
-export default ErrorPage;
